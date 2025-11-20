b13385cd9b4e71c1fef587b0e23c15ef04506062
giannag27 <giacosey28@gmail.com>
2025-11-20 09:24:15 -0500

    started creating word bank.

--- guess-the-word.py
+++ guess-the-word.py
@@ -1,1 +1,17 @@
 import random
+
+word_bank = [
+    'boba', 'pizza', 'sushi', 'burger', 'pasta', 'salad', 'taco', 'ramen',
+    'steak', 'curry', 'dumpling', 'sandwich', 'noodle', 'omelette', 'coffee',
+    'smoothie', 'waffle', 'pancake', 'fries', 'nachos', 'scone', 'bagel', 
+    'muffin', 'python', 'java', 'ruby', 'swift', 'javascript', 'html', 'css',
+    'react', 'angular', 'django', 'flask', 'node', 'linux', 'android', 
+    'typescript', 'roses', 'tulips', 'daisies', 'sunflower', 'orchid', 'lily',
+    'cactus', 'bonsai', 'fern', 'ivy', 'maple', 'oak', 'pine', 'birch', 
+    'willow', 'eagle', 'sparrow', 'parrot', 'penguin', 'dolphin', 'shark', 
+    'whale', 'tiger', 'lion', 'zebra', 'giraffe', 'elephant', 'kangaroo', 
+    'koala', 'panda', 'crocodile', 'alligator', 'butterfly', 'dragonfly', 
+    'ladybug', 'spider', 'ant', 'beetle', 'grasshopper', 'cricket', 'snail',
+    'worm', 'coral', 'jellyfish', 'seahorse', 'starfish', 'clam', 'oyster', 
+    'lobster', 'crab',
+]
